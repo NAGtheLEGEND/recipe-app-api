09442ea35b3aa510261a75cdcd62124fec1cfac2
NAG <ign.nag@gmail.com>
2024-08-26 13:23:59 +0530

    SimpletestCase demo

---
+++ app/app/calc.py
@@ -0,0 +1,5 @@
+def add(x,y):
+    return x + y
+
+def subs(x,y):
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+from app import calc
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+
+    def test_sub_nums(self):
+        res = calc.subs(1,10)
+        self.assertEqual(res,5)
