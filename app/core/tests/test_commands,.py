1e6f3daa0c85ceeb714b0089ad8eb4c87a94368b
NAG <ign.nag@gmail.com>
2024-09-02 12:13:33 +0530

    Added core app

--- app/core/tests/test_commands,.py
+++ app/core/tests/test_commands,.py
@@ -21,4 +21,14 @@ class CommandTests(SimpleTestCase)
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(database=['default'])
-        
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+            """test waiting for db when getting operational error""""
+            patched_check.side_effect = [Psycopg2Error] * 2 + \
+                [OperationalError] * 3 + [True]
+            
+            call_command('wait_for_db')
+
+            self.assertEqual(patched_check.call_count, 6)
+            patch_check.assert_called_with(database=['default'])
+
