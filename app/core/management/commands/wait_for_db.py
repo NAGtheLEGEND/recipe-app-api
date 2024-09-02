296eac0ac7ef590d6e3e60ff80e03550c0bc8e23
NAG <ign.nag@gmail.com>
2024-09-02 11:49:15 +0530

    Added core app

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+""""
+Django command to wait for database to be available.
+""""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """"Django command to wait for db. """"
+
+    def handle(self, *args, **options):
+        pass
